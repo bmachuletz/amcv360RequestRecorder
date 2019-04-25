f7db6311b1d22b1b0bf6c93dfd0c8fd716ab915d
Benjamin Machuletz <bmachuletz@mnetworx.de>
2019-04-25 15:03:38 +0200

    Changed MainForm

--- amcv/MainForm.Designer.cs
+++ amcv/MainForm.Designer.cs
@@ -46,8 +46,8 @@
             this.label5 = new System.Windows.Forms.Label();
             this.SendAnalyzedDataButton = new System.Windows.Forms.Button();
             this.listView2 = new System.Windows.Forms.ListView();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.typeHeaderUnknown = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dataHeaderUnknown = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.label6 = new System.Windows.Forms.Label();
             this.flowLayoutPanel2.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
@@ -242,21 +242,21 @@
             this.listView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2});
+            this.typeHeaderUnknown,
+            this.dataHeaderUnknown});
             this.listView2.Location = new System.Drawing.Point(3, 626);
             this.listView2.Name = "listView2";
             this.listView2.Size = new System.Drawing.Size(1184, 214);
             this.listView2.TabIndex = 14;
             this.listView2.UseCompatibleStateImageBehavior = false;
             // 
-            // columnHeader1
+            // typeHeaderUnknown
             // 
-            this.columnHeader1.Text = "Type";
+            this.typeHeaderUnknown.Text = "Type";
             // 
-            // columnHeader2
+            // dataHeaderUnknown
             // 
-            this.columnHeader2.Text = "Data";
+            this.dataHeaderUnknown.Text = "Data";
             // 
             // label6
             // 
@@ -311,8 +311,8 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.ListView listView2;
-        private System.Windows.Forms.ColumnHeader columnHeader1;
-        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader typeHeaderUnknown;
+        private System.Windows.Forms.ColumnHeader dataHeaderUnknown;
     }
 }
 
