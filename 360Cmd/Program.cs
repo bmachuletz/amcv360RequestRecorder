f9ea1023f996d9ce7cd8e4fdae57eb778a2e1a5d
Benjamin Machuletz <benjamin.machuletz@prosozial.de>
2019-09-09 09:39:53 +0200

    Commandline example-project added

---
+++ 360Cmd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CommandLine;
+using amcv;
+
+namespace _360Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Options
+    {
+        [Option('f', "requestfile", Required = true, HelpText = "Full path of the request-file.")]
+        public string RequestFile { get; set; }
+        [Option('c', "cmd", Required = true, HelpText = "COmmand to be executed (start, stop, charge).")]
+        public string Cmd { get; set; }
+    }
+}
