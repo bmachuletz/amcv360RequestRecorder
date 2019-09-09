f1f5858de5325b1761ee06d78895066c0cb1d126
Benjamin Machuletz <benjamin.machuletz@prosozial.de>
2019-09-09 09:28:13 +0200

    Revert "360Cmd.exe - example project added"
    This reverts commit a47894f087944afa2dc771234443030812729849.

--- amcv/WebEngine.cs
+++ amcv/WebEngine.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Org.BouncyCastle.Asn1.Ocsp;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace amcv
 {
