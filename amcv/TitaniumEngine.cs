fa34186f25212ae8a26c1af034542a512d652478
Benjamin Machuletz <benjamin.machuletz@prosozial.de>
2019-09-09 09:19:33 +0200

    unused reference removed

--- amcv/TitaniumEngine.cs
+++ amcv/TitaniumEngine.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 
 using Titanium.Web.Proxy;
 using Titanium.Web.Proxy.EventArguments;
-using Titanium.Web.Proxy.Http;
 using Titanium.Web.Proxy.Models;
 
 namespace amcv
